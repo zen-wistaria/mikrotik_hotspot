26ce26b82cc7c625207bc405181b17b12329df82
zen-wistaria <anri26049@gmail.com>
2026-04-18 02:37:56 +0700

    fix: change tailwind to v3

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./*.html",
+    "./js/*.js",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'neon-blue': '#00d2ff',
+        'neon-purple': '#9d50bb',
+        'dark-primary': '#0a0b10',
+        'dark-secondary': '#1a1b23',
+      },
+      animation: {
+        'fade-in-up': 'fadeInUp 0.6s cubic-bezier(0.16, 1, 0.3, 1)',
+        'glow-pulse': 'glowPulse 2s infinite ease-in-out',
+        'float': 'float 6s ease-in-out infinite',
+      },
+    },
+  },
+}
